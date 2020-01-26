13fd5911b220f487263e299945dab3f2e6595b8a
Anna Romanets <anna.romanets@tieto.com>
2020-01-26 23:49:46 +0200

    Arrays tasks current version

--- src/main/java/com/epam/lab/arrayscollections/Task2Arrays.java
+++ src/main/java/com/epam/lab/arrayscollections/Task2Arrays.java
@@ -1,5 +1,6 @@
 package com.epam.lab.arrayscollections;
 
+import com.epam.lab.logging.Application;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -8,21 +9,19 @@ import java.util.*;
 public class Task2Arrays {
     private static Logger logger1 = LogManager.getLogger(Task2Arrays.class);
 
-    public static List<Integer>  findCommon(Integer[] array1, Integer[] array2){
+    public static List<Integer> findCommon(Integer[] array1, Integer[] array2) {
         List<Integer> commonElemets = new ArrayList<>();
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array2.length; j++) {
-                if(array1[i] == array2[j]){
-                    if(!commonElemets.contains(array1[i])) {
-                        commonElemets.add(array1[i]);
-                    }
+                if (array1[i].equals(array2[j]) && !commonElemets.contains(array1[i])) {
+                    commonElemets.add(array1[i]);
                 }
             }
         }
         return commonElemets;
     }
 
-    public static List<Integer> findUnique(Integer[] array1, Integer[] array2){
+    public static List<Integer> findUnique(Integer[] array1, Integer[] array2) {
         List<Integer> uniqueElements = new ArrayList<>();
         Set<Integer> set = new HashSet<>();
         set.addAll(Arrays.asList(array1));
@@ -32,17 +31,18 @@ public class Task2Arrays {
         return uniqueElements;
     }
 
-    public static List<Integer> deleteCommon(Integer[] array){
+    public static List<Integer> deleteCommon(Integer[] array) {
         List<Integer> list = new LinkedList<>(Arrays.asList(array));
 
         return list;
     }
+
     public static void main(String[] args) {
         //A...Дано два масиви. Сформувати третій масив, що складається з тих елементів, які: а) присутні в обох масивах;
         // б) присутні тільки в одному з масивів.
         Integer[] array1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         Integer[] array2 = {6, 7, 8, 9, 10, 11, 12, 13, 14};
-        List<Integer> commonElemets = new ArrayList<>();
+        List<Integer> commonElemets;
         commonElemets = findCommon(array1, array2);
         logger1.info("Common elements:");
         commonElemets.forEach(logger1::info);
@@ -55,7 +55,7 @@ public class Task2Arrays {
         //C. Знайти в масиві всі серії однакових елементів, які йдуть підряд, і видалити з них всі елементи крім одного.
         Integer[] array3 = {1, 2, 2, 2, 5, 6, 7, 7, 9, 10};
         logger1.info("Delete common :");
-        deleteCommon(array3).forEach(logger1::info);
+        //deleteCommon(array3).forEach(logger1::info);
 
     }
 }
