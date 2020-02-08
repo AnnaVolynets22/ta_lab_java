3a5988eadd1b9d9539f7f1f55dbc4fbe4ef61646
Anna Volynets <an.volinetz@gmail.com>
2020-02-09 01:08:14 +0200

    Add string task1.

--- src/main/java/com/epam/lab/stringshw/task1regex/Main.java
+++ src/main/java/com/epam/lab/stringshw/task1regex/Main.java
@@ -9,14 +9,15 @@ import java.util.regex.Pattern;
 
 public class Main {
     private static final Logger logger = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) {
         String pattern = "^[A-Z]+[\\w\\d\\s]*[.]$";
-        String sentence = "Same sentence.";
         List<String> testData = List.of("Same sentence.", "not sentence", "Sentence without period");
         Pattern p = Pattern.compile(pattern);
 
-        testData.forEach(s ->{Matcher m = p.matcher(s);
-        if(m.matches()) logger.info(String.format("Sentence which matches: %s", s));
+        testData.forEach(s -> {
+            Matcher m = p.matcher(s);
+            if (m.matches()) logger.info(String.format("Sentence which matches: %s", s));
         });
 
     }
